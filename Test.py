9357fbbd0670b25e02a9d67ced2d6831776ef618
RickJacome <48185556+RickJacome@users.noreply.github.com>
2024-06-19 21:26:23 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,5 +1,1 @@
-print( 'Page is Ready!')
-import numpy as np
-a = np.random.randint(1, 5)
-b = np.random.randint(4, 10)
-print('if a = ' + str(a) + ' and b = ' + str(b) + '. What is a + b = ? ')
+print('What is a + b = ? ')
