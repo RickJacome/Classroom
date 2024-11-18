51f22ce7437b9b580a5c02f7e4603ab13b5f1cd9
RickJacome <48185556+RickJacome@users.noreply.github.com>
2024-11-18 09:41:31 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -21,10 +21,8 @@ v0 = 0.0  # Initial velocity
 omega0 = np.sqrt(k/m)
 zeta = b/(2*np.sqrt(k*m))
 omega = omega0 * np.sqrt(1 - zeta**2)
-x = x0 * np.exp(-zeta * omega0 * t) * np.cos(omega * t - np.arctan(zeta / np.sqrt(1 - zeta**2)))
-
-# Time array
 t = np.linspace(0, 10, 1000)
+x = x0 * np.exp(-zeta * omega0 * t) * np.cos(omega * t - np.arctan(zeta / np.sqrt(1 - zeta**2)))
 
 # Calculate displacement
 #x = underdamped_shm(m, k, b, x0, v0, t)
