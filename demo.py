e0c8eda6cb85291441fef32499447f002be599d9
RickJacome <48185556+RickJacome@users.noreply.github.com>
2024-11-18 11:47:42 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,14 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#def underdamped_shm(m, k, b, x0, v0, t):
-#    """Simulates underdamped simple harmonic motion."""
-#    omega0 = np.sqrt(k/m)
-#    zeta = b/(2*np.sqrt(k*m))
-#    omega = omega0 * np.sqrt(1 - zeta**2)
-#
-#    x = x0 * np.exp(-zeta * omega0 * t) * np.cos(omega * t - np.arctan(zeta / np.sqrt(1 - zeta**2)))
-#    return x
+def underdamped_shm(m, k, b, x0, v0, t):
+    """Simulates underdamped simple harmonic motion."""
+    omega0 = np.sqrt(k/m)
+    zeta = b/(2*np.sqrt(k*m))
+    omega = omega0 * np.sqrt(1 - zeta**2)
+    x = x0 * np.exp(-zeta * omega0 * t) * np.cos(omega * t - np.arctan(zeta / np.sqrt(1 - zeta**2)))
+    return x
 #def plotting():
 # Parameters
 m = 1.0  # Mass
@@ -16,20 +15,20 @@ k = 10.0  # Spring constant
 b = 0.5  # Damping coefficient
 x0 = 1.0  # Initial displacement
 v0 = 0.0  # Initial velocity
-omega0 = np.sqrt(k/m)
-zeta = b/(2*np.sqrt(k*m))
-omega = omega0 * np.sqrt(1 - zeta**2)
-t = np.linspace(0, 10, 1000)
-x = x0 * np.exp(-zeta * omega0 * t) * np.cos(omega * t - np.arctan(zeta / np.sqrt(1 - zeta**2)))
 
+t = np.linspace(0, 10, 1000)
 # Calculate displacement
-#x = underdamped_shm(m, k, b, x0, v0, t)
+x = underdamped_shm(m, k, b, x0, v0, t)
 # Plotting
-fig, ax = plt.subplots(figsize=(10,2))
-plt.plot(t, x)
+fig, ax = plt.subplots(1,3, figsize=(10,3))
+
+ax[0].plot(t, x)
+ax[1].plot(t, x)
+ax[2].plot(t, x)
+
 plt.xlabel('Time (s)'); 
 plt.ylabel('Displacement (m)')
-plt.title('Underdamped Simple Harmonic Motion');
+plt.title('Underdamped');
 
 #display(fig, target="graph-area")
 fig
