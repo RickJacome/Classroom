2398bb33c6c8a057a149ddfa4521485330565a12
RickJacome <48185556+RickJacome@users.noreply.github.com>
2024-11-19 21:53:18 -0500

    Update demo2.py

--- demo2.py
+++ demo2.py
@@ -11,11 +11,15 @@ y = np.linspace(-6, 6, 30)
 X, Y = np.meshgrid(x, y)
 Z = f(X, Y)
 
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-ax.contour3D(X, Y, Z, 50, cmap='binary')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z');
+#fig = plt.figure()
+#ax = plt.axes(projection='3d')
+
+fig, ax = plt.subplots(1,3, dpi=120,figsize=(10,2), constrained_layout=True)
+ax[0] = plt.axes(projection='3d')
+
+ax[0].contour3D(X, Y, Z, 50, cmap='binary')
+ax[0].set_xlabel('x')
+ax[0].set_ylabel('y')
+ax[0].set_zlabel('z');
 
 fig
