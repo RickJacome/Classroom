3c432288e45380db844f7554333b2b9d2282a122
RickJacome <48185556+RickJacome@users.noreply.github.com>
2024-12-12 17:53:04 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -17,20 +17,21 @@ x0 = 1.0  # Initial displacement
 v0 = 0.0  # Initial velocity
 
 t = np.linspace(0, 10, 1000)
-# Calculate displacement
 x = underdamped_shm(m, k, b, x0, v0, t)
+dx_dt = np.diff(x)/np.diff(t)
+
+fft_result = np.fft.fft(x)
+freq = np.fft.fftfreq(len(x), 0.01)
 # Plotting
 fig, ax = plt.subplots(1,3, dpi=120,figsize=(10,2), constrained_layout=True)
 
 ax[0].plot(t, x)
-ax[1].plot(t, x)
-ax[2].plot(t, x)
+ax[1].plot(t, dx_dt)
+ax[2].plot(freq, fft_result)
 
-ax[0].set_xlabel('Time (s)');  ax[0].set_ylabel('Displacement (m)'); ax[0].set_title('Underdamped');
-
-ax[1].set_xlabel('Time (s)');  ax[1].set_ylabel('Displacement (m)'); ax[1].set_title('Damped');
-
-ax[2].set_xlabel('Time (s)');  ax[2].set_ylabel('Displacement (m)'); ax[2].set_title('Underdamped');
+ax[0].set_xlabel('Time (s)');  ax[0].set_ylabel('Displacement (m)'); ax[0].set_title('Theoretical Underdamped System');
+ax[1].set_xlabel('Time (s)');  ax[1].set_ylabel('Velocity (mm/s)'); ax[1].set_title('Derivative of Theoretical System');
+ax[2].set_xlabel('Frequency (Hz)');  ax[2].set_ylabel('Amplitude'); ax[2].set_title('Fast Fourier Transform');
 
 fig
 
