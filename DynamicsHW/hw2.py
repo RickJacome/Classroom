96914ee8d5685fc5431415d7dc138f1d3130509b
RickJacome <48185556+RickJacome@users.noreply.github.com>
2025-03-04 10:04:18 -0500

    Update hw2.py

--- DynamicsHW/hw2.py
+++ DynamicsHW/hw2.py
@@ -196,7 +196,11 @@ def final_sub(*ags, **kws):
 	out8 = Element("outputDiv8")
 	out9 = Element("outputDiv9")
 
-	grade = round(((pnt1+pnt2+pnt3+pnt4+pnt5+pnt6+pnt7+pnt8+pnt9)/9)*100,1)
 	name = Element('student-name')
 	out_final = Element('outputFinal')
+	try:
+		round(((pnt1+pnt2+pnt3+pnt4+pnt5+pnt6+pnt7+pnt8+pnt9)/9)*100,1)
+	except NameError:
+		out_final.write(f"You did not finish submitting all questions above. Refresh Page and start again. ")
+	grade = round(((pnt1+pnt2+pnt3+pnt4+pnt5+pnt6+pnt7+pnt8+pnt9)/9)*100,1)
 	out_final.write(f" Thank you {name.value}, your answers have been submitted. Your score is: {grade} %")
